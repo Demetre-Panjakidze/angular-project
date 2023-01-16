5c6adf43ae7067b514c3b7e1ed103846e203d73b
Demetre Panjakidze <Dpanj2021@agruni.edu.ge>
2023-01-17 00:55:32 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 
+function capitalize(s:string) {
+    return s[0].toUpperCase() + s.slice(1);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+  tasks_and_levels: any[] = [];
+
+  taskName: string = "";
+  selectedOption: string = "";
+  options: string[] = ['easy', 'medium', 'hard'];
+
+  addTask = () => {
+    if(this.taskName && this.options.includes(this.selectedOption)) {
+      this.tasks_and_levels.push([this.taskName, capitalize(this.selectedOption)]);
+      this.taskName = "";
+      this.selectedOption = "";
+    }
+  }
+
+  deleteLi = (index: number) => {
+    this.tasks_and_levels = this.tasks_and_levels.filter((_:unknown, i:unknown) => index !== i);
+  }
+
+  middle_tasks_and_levels: any[] = [];
+
+  rightTransfer = (index: number) => {
+    this.middle_tasks_and_levels.push([this.taskName, this.selectedOption]);
+    console.log(this.middle_tasks_and_levels);
+  }
+
+  middleRightTransfer = (index:number) => {
+
+  }
+
+  leftTransfer = (index: number) => {
+    
+  }
+
+  endLeftTransfer = (index: number) => {
+    
+  }
 }
